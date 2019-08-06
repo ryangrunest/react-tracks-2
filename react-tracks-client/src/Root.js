c5bc102eda0750fcc57b0ebd06877f214ef484ac
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-06 08:43:39 -0700

    Routing with React Router

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,8 +1,14 @@
 import React from "react";
 import withRoot from "./withRoot";
-
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost'
+
+import App from './pages/App';
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 const Root = () => (
   <Query query={ME_QUERY}>
@@ -10,7 +16,17 @@ const Root = () => (
       if (loading) return <div>Loading</div>
       if (error) return <div>Error</div>
 
-      return <div>{JSON.stringify(data)}</div>
+      return (
+        <Router>
+          <>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/profile/:id" component={Profile}/>
+          </Switch>
+          </>
+        </Router>
+      )
     }}
   </Query>
 )
@@ -25,15 +41,15 @@ const ME_QUERY = gql`
     }
   }  
 `
-const GET_TRACKS_QUERY = gql`
-  {
-    tracks {
-      id
-      title
-      description
-      url
-    }
-  }
-`;
+// const GET_TRACKS_QUERY = gql`
+//   {
+//     tracks {
+//       id
+//       title
+//       description
+//       url
+//     }
+//   }
+// `;
 
 export default withRoot(Root);
