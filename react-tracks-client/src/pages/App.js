80b312c06ef4b7f7277519dd764d4362051d0d9f
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-19 09:35:42 -0700

    deleted tracks from db without user id, created tracklist component

--- react-tracks-client/src/pages/App.js
+++ react-tracks-client/src/pages/App.js
@@ -1,26 +1,56 @@
-// import React from "react";
-// import withStyles from "@material-ui/core/styles/withStyles";
+import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-// import SearchTracks from "../component/Track/SearchTracks";
-// import TrackList from "../component/Track/TrackList";
-// import CreateTrack from "../component/Track/CreateTrack";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
 
-// const App = ({ classes }) => {
-//   return (
-//     <div className={classes.container}>
-//       <SearchTracks />
-//       <TrackList />
-//       <CreateTrack />
-//     </div>
-//   );
-// };
+import SearchTracks from "../components/Track/SearchTracks";
+import TrackList from "../components/Track/TrackList";
+import CreateTrack from "../components/Track/CreateTrack";
+import Loading from "../components/Shared/Loading";
+import Error from "../components/Shared/Error";
 
-// const styles = theme => ({
-//   container: {
-//     margin: "0 auto",
-//     maxWidth: 960,
-//     padding: theme.spacing.unit * 2
-//   }
-// });
+const App = ({ classes }) => {
+  return (
+    <div className={classes.container}>
+      <SearchTracks />
+      <CreateTrack />
+      <Query query={GET_TRACKS_QUERY}>
+        {({ data, loading, error }) => {
+          if (loading) return <Loading />;
+          if (error) return <Error error={error} />;
 
-// export default withStyles(styles)(App);
+          return <TrackList tracks={data.tracks} />;
+        }}
+      </Query>
+    </div>
+  );
+};
+
+const GET_TRACKS_QUERY = gql`
+  query getTracksQuery {
+    tracks {
+      id
+      title
+      description
+      url
+      likes {
+        id
+      }
+      postedBy {
+        id
+        username
+      }
+    }
+  }
+`;
+
+const styles = theme => ({
+  container: {
+    margin: "0 auto",
+    maxWidth: 960,
+    padding: theme.spacing.unit * 2
+  }
+});
+
+export default withStyles(styles)(App);
