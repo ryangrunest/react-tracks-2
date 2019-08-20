9e8dd9cddf701559cfae31a1826fa69220893ab5
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-20 13:24:56 -0700

    updating track list according to search results

--- react-tracks-client/src/components/Track/SearchTracks.js
+++ react-tracks-client/src/components/Track/SearchTracks.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { ApolloConsumer } from "react-apollo";
 import { gql } from "apollo-boost";
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -8,8 +8,9 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 
-const SearchTracks = ({ classes }) => {
+const SearchTracks = ({ classes, setSearchResults }) => {
   const [search, setSearch] = useState("");
+  const inputEl = useRef();
 
   const handleSubmit = async (event, client) => {
     event.preventDefault();
@@ -17,7 +18,13 @@ const SearchTracks = ({ classes }) => {
       query: SEARCH_TRACKS_QUERY,
       variables: { search }
     });
-    console.log({ res });
+    setSearchResults(res.data.tracks);
+  };
+
+  const clearSearchInput = () => {
+    setSearchResults([]);
+    setSearch("");
+    inputEl.current.focus();
   };
 
   return (
@@ -25,7 +32,7 @@ const SearchTracks = ({ classes }) => {
       {client => (
         <form onSubmit={event => handleSubmit(event, client)}>
           <Paper className={classes.root} elevation={1}>
-            <IconButton>
+            <IconButton onClick={clearSearchInput}>
               <ClearIcon />
             </IconButton>
             <TextField
@@ -35,6 +42,8 @@ const SearchTracks = ({ classes }) => {
                 disableUnderline: true
               }}
               onChange={event => setSearch(event.target.value)}
+              value={search}
+              inputRef={inputEl}
             />
             <IconButton type="submit">
               <SearchIcon />
