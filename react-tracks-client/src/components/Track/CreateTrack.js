9de9b1f4acce82b3ae4a296b6185e72e2d127c8d
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-19 22:47:34 -0700

    add tracks to cloudinary media server

--- react-tracks-client/src/components/Track/CreateTrack.js
+++ react-tracks-client/src/components/Track/CreateTrack.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Dialog from "@material-ui/core/Dialog";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import axios from "axios";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
@@ -14,15 +17,44 @@ import AddIcon from "@material-ui/icons/Add";
 import ClearIcon from "@material-ui/icons/Clear";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
+import Error from "../Shared/Error";
+
 const CreateTrack = ({ classes }) => {
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [file, setFile] = useState("");
+  const [submitting, setSubmitting] = useState(false);
 
   const handleAudioChange = event => {
     const selectedFile = event.target.files[0];
     setFile(selectedFile);
+  };
+
+  const handleAudioUpload = async () => {
+    try {
+      const data = new FormData();
+      data.append("file", file);
+      data.append("resource_type", "raw");
+      data.append("upload_preset", "react-tracks");
+      data.append("cloud_name", "ryangrunest");
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/ryangrunest/raw/upload",
+        data
+      );
+      return res.data.url;
+    } catch (err) {
+      console.error("Error uploading file", err);
+      setSubmitting(false);
+    }
+  };
+
+  const handleSubmit = async (event, createTrack) => {
+    event.preventDefault();
+    setSubmitting(true);
+    // upload audio files, get returned url from api
+    const uploadedUrl = await handleAudioUpload();
+    createTrack({ variables: { title, description, url: uploadedUrl } });
   };
 
   return (
@@ -38,67 +70,112 @@ const CreateTrack = ({ classes }) => {
       </Button>
 
       {/* Create Track Dialog */}
-      <Dialog open={open} className={classes.dialog}>
-        <form>
-          <DialogTitle>Create Track</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Add a Title, Description, and Audio File
-            </DialogContentText>
-            <FormControl fullWidth>
-              <TextField
-                label="Title"
-                placeholder="Add Title"
-                onChange={event => setTitle(event.target.value)}
-                className={classes.textField}
-              />
-            </FormControl>
-            <FormControl fullWidth>
-              <TextField
-                multiline
-                rows="2"
-                label="Description"
-                placeholder="Add Description"
-                onChange={event => setDescription(event.target.value)}
-                className={classes.textField}
-              />
-            </FormControl>
-            <FormControl fullWidth>
-              <input
-                id="audio"
-                required
-                type="file"
-                accept="audio/mp3, audio/wav"
-                className={classes.input}
-                onChange={handleAudioChange}
-              />
-              <label htmlFor="audio">
-                <Button
-                  variant="outlined"
-                  color="inherit"
-                  component="span"
-                  className={classes.button}
-                >
-                  Audio File
-                  <LibraryMusicIcon className={classes.icon} />
-                </Button>
-                {file && file.name}
-              </label>
-            </FormControl>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setOpen(false)} className={classes.cancel}>
-              Cancel
-            </Button>
-            <Button type="submit" className={classes.save}>
-              Add Track
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
+      <Mutation
+        mutation={CREATE_TRACK_MUTATION}
+        onCompleted={data => {
+          console.log({ data });
+          setSubmitting(false);
+          setOpen(false);
+        }}
+      >
+        {(createTrack, { loading, error }) => {
+          if (error) return <Error error={error} />;
+
+          return (
+            <Dialog open={open} className={classes.dialog}>
+              <form onSubmit={event => handleSubmit(event, createTrack)}>
+                <DialogTitle>Create Track</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Add a Title, Description, and Audio File
+                  </DialogContentText>
+                  <FormControl fullWidth>
+                    <TextField
+                      label="Title"
+                      placeholder="Add Title"
+                      onChange={event => setTitle(event.target.value)}
+                      className={classes.textField}
+                    />
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <TextField
+                      multiline
+                      rows="2"
+                      label="Description"
+                      placeholder="Add Description"
+                      onChange={event => setDescription(event.target.value)}
+                      className={classes.textField}
+                    />
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <input
+                      id="audio"
+                      required
+                      type="file"
+                      accept="audio/mp3, audio/wav"
+                      className={classes.input}
+                      onChange={handleAudioChange}
+                    />
+                    <label htmlFor="audio">
+                      <Button
+                        variant="outlined"
+                        color={file ? "secondary" : "inherit"}
+                        component="span"
+                        className={classes.button}
+                      >
+                        Audio File
+                        <LibraryMusicIcon className={classes.icon} />
+                      </Button>
+                      {file && file.name}
+                    </label>
+                  </FormControl>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    disabled={submitting}
+                    onClick={() => setOpen(false)}
+                    className={classes.cancel}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    disabled={
+                      submitting ||
+                      !title.trim() ||
+                      !description.trim() ||
+                      !file
+                    }
+                    type="submit"
+                    className={classes.save}
+                  >
+                    {submitting ? (
+                      <CircularProgress className={classes.save} size={24} />
+                    ) : (
+                      "Add Track"
+                    )}
+                  </Button>
+                </DialogActions>
+              </form>
+            </Dialog>
+          );
+        }}
+      </Mutation>
     </>
   );
 };
+
+const CREATE_TRACK_MUTATION = gql`
+  mutation($title: String!, $description: String!, $url: String!) {
+    createTrack(title: $title, description: $description, url: $url) {
+      track {
+        id
+        title
+        description
+        url
+      }
+    }
+  }
+`;
 
 const styles = theme => ({
   container: {
