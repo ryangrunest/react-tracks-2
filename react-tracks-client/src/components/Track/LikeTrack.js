b38bb5e176dc11a5e48a8100d96c91b7f73b0c25
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-21 16:38:28 -0700

    disable more than one like

--- react-tracks-client/src/components/Track/LikeTrack.js
+++ react-tracks-client/src/components/Track/LikeTrack.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Mutation } from "react-apollo";
 import { gql } from "apollo-boost";
 import withStyles from "@material-ui/core/styles/withStyles";
 import IconButton from "@material-ui/core/IconButton";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
+import { UserContext, ME_QUERY } from "../../Root";
+
 const LikeTrack = ({ classes, trackId, likeCount }) => {
+  const currentUser = useContext(UserContext);
+
+  const handleDisableLikedTrack = () => {
+    const userLikes = currentUser.likeSet;
+    const isTrackLiked =
+      userLikes.findIndex(({ track }) => track.id === trackId) > -1;
+    return isTrackLiked;
+  };
+
   return (
     <Mutation
       mutation={CREATE_LIKE_MUTATION}
@@ -13,6 +24,7 @@ const LikeTrack = ({ classes, trackId, likeCount }) => {
       onCompleted={data => {
         console.log({ data });
       }}
+      refetchQueries={() => [{ query: ME_QUERY }]}
     >
       {createLike => (
         <IconButton
@@ -21,6 +33,7 @@ const LikeTrack = ({ classes, trackId, likeCount }) => {
             event.stopPropagation();
             createLike();
           }}
+          disabled={handleDisableLikedTrack()}
         >
           {likeCount}
           <ThumbUpIcon className={classes.icon} />
